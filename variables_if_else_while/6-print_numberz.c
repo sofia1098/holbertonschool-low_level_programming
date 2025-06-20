0cd9c38a8541c86c58bef8310eae8575bac17970
sofia1098 <sofianunez000@gmail.com>
2025-06-20 15:47:36 +0000

    seis

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - checkea si un numero es positivo, negativo o 0
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+
+	printf("\n");
+	return (0);
+}
