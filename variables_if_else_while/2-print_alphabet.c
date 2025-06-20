3085bc840db2d6b71eb0d662abe8d796fbe16d52
sofia1098 <sofianunez000@gmail.com>
2025-06-20 14:21:55 +0000

    dos

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Abecedario con putchar
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char letra = 'a';
+
+	int i = 0;
+
+	for (i = 0; i <= 26; i++)
+		putchar(letra + i);
+
+	return (0);
+}
