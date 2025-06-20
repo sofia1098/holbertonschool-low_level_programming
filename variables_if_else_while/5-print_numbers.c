d39bfd6f65d409c7dd6bd7d0f16f01c0835f4225
sofia1098 <sofianunez000@gmail.com>
2025-06-20 15:17:17 +0000

    cinco

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - checkea si un numero es positivo, negativo o 0
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+	return (0);
+}
