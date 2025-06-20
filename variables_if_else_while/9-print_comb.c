a3ee22f91210ce1d4a4b774744cff59788aca883
sofia1098 <sofianunez000@gmail.com>
2025-06-20 16:23:00 +0000

    nueve

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - checkea si un numero es positivo, negativo o 0
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
