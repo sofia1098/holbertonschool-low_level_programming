143d2c046a58e30028eb7d0a2af3283bb606afc1
sofia1098 <sofianunez000@gmail.com>
2025-06-20 17:01:20 +0000

    ultimo

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,11 +9,15 @@ int main(void)
 {
 	int num = 0;
 
-	for (num = 0; num < 10; num++)
+	while (num < 10)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
 	}
 	putchar('\n');
 	return (0);
