df8e6a6bed94d2bcec06bd0451f2ccf72a6ebb97
sofia1098 <sofianunez000@gmail.com>
2025-06-25 17:11:54 +0000

    cinco

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * more_numbres - 
- * prints 10 times the numbers, from 0 to 14, followed by a new line.
- * 
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
  *  Return: void
  */
 
 void more_numbers(void)
 {
+	/*"01234567891011121314\0"*/
+	char *p = "01234567891011121314";
+
+	int i;
+
+	int x;
+
+	for (x = 0; x < 11; x++)
+	{
+		for (i = 0; p[i] != '\0'; i++)
+		{
+			_putchar(p[i]);
+		}
+	_putchar('\n');
+	}
 }
