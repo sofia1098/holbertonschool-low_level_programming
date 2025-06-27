75d4f02a82fa9973868842e2db804a652bafe5d2
sofia1098 <sofianunez000@gmail.com>
2025-06-27 14:27:01 +0000

    nuve

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizz_buzz - prints the numbers from 1 to 100.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", a);
+	}
+	printf("\n");
+	return 0;
+}
