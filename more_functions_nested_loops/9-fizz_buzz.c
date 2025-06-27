71a739ffc4053a81e98b2f1730868a75866dc6fe
sofia1098 <sofianunez000@gmail.com>
2025-06-27 14:52:07 +0000

    nueve

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,8 +15,10 @@ int main(void)
 	{
 		if (a % 5 == 0 && a % 3 == 0)
 			printf("FizzBuzz ");
-		else if (a % 3 == 0)
+		else if (a % 3 == 0 && a != 100)
 			printf("Fizz ");
+		else if (a == 100)
+			printf("Buzz");
 		else if (a % 5 == 0)
 			printf("Buzz ");
 		else
