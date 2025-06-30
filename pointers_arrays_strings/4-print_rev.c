0cad82e3262c31556edb36a4c17cacb58d6f2a70
sofia1098 <sofianunez000@gmail.com>
2025-06-30 18:10:32 +0000

    cuatro

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
+
+/**
+ * print_rev - print a reverse string
+ * @s: string to be printed
+ */
+
 void print_rev(char *s)
 {
 	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++);
-	
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	i--;
 	for (j = i; j >= 0; j--)
 		_putchar(s[j]);
-	_putchar('\n');	
+	_putchar('\n');
 }
