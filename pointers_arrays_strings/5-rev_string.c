79c6ec389fb0aca4e1e2a611a342a92156cf4e4f
sofia1098 <sofianunez000@gmail.com>
2025-07-01 14:22:26 +0000

    cinco

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,11 +5,15 @@ rev_string(char *s)
 {
 	int i, j;
 
+	char k;
+
 	for (i = 0; s[i] != '\0'; i++)
 		;
-	for (j = i; j >= 0; j--)
+	i--;
+	for (j = 0; j < i; j++, i--)
 	{
-		_putchar(s[j]);
+		k = s[j];
+		s[j] = s[i];
+		s[i] = k;
 	}
-	_putchar('\n');
 }
