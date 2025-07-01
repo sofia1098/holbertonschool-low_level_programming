7afa2dd78e5e67947e9df5b94d2096cefdd54ccb
sofia1098 <sofianunez000@gmail.com>
2025-07-01 16:04:04 +0000

    ocho

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+void
+print_array(int *a, int n)
+{
+	int i;
+
+		for (i = 0; i <= n ; i++)
+		{
+			if (i == n)
+				printf("%d", a[i]);
+			printf("%d, ", a[i]);
+		}
+	printf("\n");
+}
