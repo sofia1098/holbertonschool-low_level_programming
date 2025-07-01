9ae84ac426ba4b96f5bd3049d9ed913a36a6a736
sofia1098 <sofianunez000@gmail.com>
2025-07-01 15:11:42 +0000

    siete

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,11 +7,11 @@ puts_half(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	if ((i % 2) != 0)
+	if ((i % 2) == 0)
 		for (mitad = (i / 2); str[mitad] != '\0' ; mitad++)
 			_putchar(str[mitad]);
-	else 
+	else
 		for (mitad = ((i + 1) / 2); str[mitad] != '\0' ; mitad++)
-                        _putchar(str[mitad]);
+		_putchar(str[mitad]);
 	_putchar('\n');
 }
