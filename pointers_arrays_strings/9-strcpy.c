2f5261d555dcede947f4b0da8b76c1a8c0032a49
sofia1098 <sofianunez000@gmail.com>
2025-07-02 14:48:31 +0000

    nueve

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,14 +5,13 @@ char
 {
 	int i;
 
-	char copia; /*char *copia = malloc(strlen(src) + 1); //
-if (copia == NULL) {*/
+	char copia;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		copia = src[i];
 		dest[i] = copia;
 	}
-	dest += '\0';
+	*dest += '\0';
 	return dest;
 }
