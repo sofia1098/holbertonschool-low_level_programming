b5ecb98d91c9b0d9d80edf95c7f659c6571d19c0
sofia1098 <sofianunez000@gmail.com>
2025-07-03 17:15:30 +0000

    betty te odio

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
-char
-*_strcat(char *dest, char *src)
+/**
+ * *_strcat - function thats order the string
+ * @dest: pointer.
+ * @src: pointer.
+ *
+ * Return: ptr
+ *
+ */
+
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
 	char *ptr = dest;
 
-	for (i = 0; dest[i] != '\0'; i++) /*ponde el indice en el final del string destino*/
+	for (i = 0; dest[i] != '\0'; i++) /* ponde el indice en el */
+					  /* final del string destino */
 		;
 	for (j = 0; src[j] != '\0'; j++) /*copia desde el final */
 	{
