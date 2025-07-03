cd0afbd5758faf367aa7dfac305102f80038d444
sofia1098 <sofianunez000@gmail.com>
2025-07-03 17:53:38 +0000

    uno con error

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+char
+*_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++) /* ponde el indice en el */
+					  /* final del string destino */
+		;
+	for (j = 0; j < n || src[j] != '\0'; j++) /*copia desde el final */
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
