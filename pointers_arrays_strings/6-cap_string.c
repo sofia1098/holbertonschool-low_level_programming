c32b890d8a102d97e1b9cef8f9e2a077a7aa25da
sofia1098 <sofianunez000@gmail.com>
2025-07-04 18:19:46 +0000

    prueba 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,15 @@
 #include "main.h"
 
+char *cap_string(char *str)
+{
+	int i, j;
 
+	char a[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0';i++)
+	{
+		if (str[i] > 'a' && str[i] < 'z' && str[i - 1] == a[])
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
