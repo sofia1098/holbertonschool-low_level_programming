e05e08112eb3cdc74efa51ef390e94fa00366bff
sofia1098 <sofianunez000@gmail.com>
2025-07-04 15:07:32 +0000

    betty te odio

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,23 +2,7 @@
 
 int
 _strcmp(char *s1, char *s2)
-{
-/**	int i, j;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	if (i > j)
-		return (i + j);
-	if (i < j)
-		return (i - j);
-	if (i == j) 
-		for (i = 0; s1[i] == s2[i]; i++)
-			return (0);
-	else
-		return (i - j);
-**/
 	int i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
