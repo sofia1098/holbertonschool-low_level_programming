99113d5ba92c28c3b98562fedd08bdb84d94c35d
sofia1098 <sofianunez000@gmail.com>
2025-07-07 13:39:06 +0000

    fallo critico

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,11 +11,13 @@ char *cap_string(char *str)
 		{
 			if (str[i] == a[j])
 			{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')               
-				str[i + 1] -= 32;
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			 	{		
+					str[i + 1] -= 32;
+				}
 			}
 		}
+		i++;
 	}
-	i++;
 	return (str);
 }
