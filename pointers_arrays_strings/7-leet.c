c21704b8d987165b7718ff756be7d177749ca98f
sofia1098 <sofianunez000@gmail.com>
2025-07-07 14:49:25 +0000

    fin

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+char
+*leet(char *s) 
+{
+	int i = 0, j;
+
+	char a[] = "aAeEoOtTlL";
+
+	char leet[] = "4433007711";
+
+	while (s[i] != '\0') /*recorre string*/
+	{
+		j = 0;
+		while (a[j] != '\0') /*recorre a*/
+		{
+			if (s[i] == a[j]) /*si s match a*/
+			{
+				s[i] = leet[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
