52f68c944da02b9b3e74a0b7e688635f63551535
sofia1098 <sofianunez000@gmail.com>
2025-07-08 16:07:59 +0000

    tres corregido

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,18 +3,22 @@
 unsigned int
 _strspn(char *s, char *accept)
 {
-	int i, j, k;
+	int i, j, match, contador = 0;
 
-	for (i = 0; s[i] != '\0'; i++) /*recoore string*/
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		j = 0;
-		while (accept[j] != '\0') /*recorre substring*/
+		match = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j]) /*find match*/
-				k++;
-			j++;
+			if (s[i] == accept[j])
+			{
+				match = 1;
+				break;
+			}
 		}
-		i++;
+	if (!match)
+		break;
+	contador ++;
 	}
-	return (k);
+	return (contador);
 }
