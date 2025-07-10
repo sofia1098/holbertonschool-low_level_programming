2957a447c8d6a1feea42ea7971e55315e9bf8a18
sofia1098 <sofianunez000@gmail.com>
2025-07-10 12:36:05 +0000

    fin

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,22 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 
-void print_diagsums(int *a, int size)
+void
+print_diagsums(int *a, int size)
 {
 	int i;
 
 	int sum1 = 0, sum2 = 0;
 
-	int *p1 = a;// Inicio (fila 0, col 0)
-	int *p2 = a + size - 1;
+	int *p1 = a; /* Inicio 0 0*/
+
+	int *p2 = a + size - 1; /*puntero al final*/
 
 	for (i = 0; i < size; i++)
 	{
 		sum1 += *p1;
 		sum2 += *p2;
-		p1 += size + 1;// Mover a sig posición de la diagonal principal
-		p2 += size - 1;// Mover a sig de la secundaria
+		p1 += size + 1;/* corre a sig posición de la diagonal principal*/
+		p2 += size - 1;/* Mover a sig de la secundaria*/
 	}
 
-	printf("%d, %d", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
