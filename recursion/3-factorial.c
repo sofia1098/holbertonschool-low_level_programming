73a575bb2f1e628b64a406392b4d4a0db950653d
sofia1098 <sofianunez000@gmail.com>
2025-07-12 19:57:55 +0000

    tres

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
-int factorial(int n)
+int
+factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+	if (n == 0)
+		return (1);
 
-	factorial(n + 1)
-	n = (n - 1)
+	else
+		return (n * factorial(n - 1));
 }
