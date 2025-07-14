8d5ce4c274d82248cca8b5655e71a91a3c292369
sofia1098 <sofianunez000@gmail.com>
2025-07-14 18:08:54 +0000

    raiz cudrada-1

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,12 +2,26 @@
 
 int _sqrt_recursion(int n)
 {
-	int x;
-
+	int aprox_raiz;
+	
 	if (n < 0)
+	{
 		return (-1);
-	else
-		n = x * x; 
-		return (n /  _sqrt_recursion(x - 1));
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
 
+	aprox_raiz = (n / _sqrt_recursion(n - 1));
+
+	if ((aprox_raiz) * (aprox_raiz) < n)
+		return (aprox_raiz + 1);
+
+	if ((aprox_raiz + 1) * (aprox_raiz + 1) > n)
+		return (-1);
+
+	return ( aprox_raiz);
+
+	/*preguntar si se puede usar funcion auxiliar*/
 }
