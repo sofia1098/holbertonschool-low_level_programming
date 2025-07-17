a772b1c5d90195cb5b149f657d0fcc70f70c3258
sofia1098 <sofianunez000@gmail.com>
2025-07-17 17:11:50 +0000

    tres actualizado

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,14 +11,16 @@ int
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	matriz = malloc(sizeof(int *) * height);
+	matriz = malloc(sizeof(int *) * height); /*espaico para filas*/
 	if (matriz == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++) /*para cada fila*/
 	{
-		matriz[i] = malloc(width * sizeof(int *));
-
+		matriz[i] = malloc(width * sizeof(int)); /*cantidad de olumnas*/	
+		if (matriz[i] == NULL)
+			return (NULL);
+		/*iniciar en 0 todos los espacios*/
 		for (j = 0; j < width; j++)
 			matriz[i][j] = 0;
 	}
