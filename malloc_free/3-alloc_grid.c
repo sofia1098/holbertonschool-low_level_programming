f5137ca862458ced117e18f0dc29619c8f5091ff
sofia1098 <sofianunez000@gmail.com>
2025-07-17 00:08:41 +0000

    sin terminar

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+int
+**alloc_grid(int width, int height)
+{
+	int i, j;
+
+	int **matriz;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	
+
+	
+}
