89dcb2c995352b2a61c3bd6e3b59bc2bc287d026
sofia1098 <sofianunez000@gmail.com>
2025-07-17 16:52:07 +0000

    tres

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,7 +11,16 @@ int
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	
+	matriz = malloc(sizeof(int *) * height);
+	if (matriz == NULL)
+		return (NULL);
 
-	
+	for (i = 0; i < width; i++)
+	{
+		matriz[i] = malloc(width * sizeof(int *));
+
+		for (j = 0; j < width; j++)
+			matriz[i][j] = 0;
+	}
+	return (matriz);
 }
