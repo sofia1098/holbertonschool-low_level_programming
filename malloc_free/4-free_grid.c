92d1db1c5ef8a946c0f81b9fa588aa7f1a1b47b0
sofia1098 <sofianunez000@gmail.com>
2025-07-18 21:54:26 +0000

    cuatro

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+void
+free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
