16557d43bbad81301eabda1ed3253ca644a56ad2
sofia1098 <sofianunez000@gmail.com>
2025-07-18 23:53:32 +0000

    cero

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
-void *malloc_checked(unsigned int b)
+void
+*malloc_checked(unsigned int b)
 {
+	void *p = malloc(b);
 
+	if (p == NULL)
+		exit (98);
+	return (p);
 }
