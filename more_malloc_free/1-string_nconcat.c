b00122738d795bfe627e0eece2b84b1ce9d9e8ce
sofia1098 <sofianunez000@gmail.com>
2025-07-19 00:16:59 +0000

    uno

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+char
+*string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1, len2;
+
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s1[len2] != '\0'; len2++)
+                ;
+
+	str = malloc(sizeof(char) * (len1 + len2));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		str[j + i] = s2[j];
+
+	str[j + i] = '\0';
+	return (str);
+
+}
