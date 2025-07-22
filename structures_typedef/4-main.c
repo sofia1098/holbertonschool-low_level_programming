3f403e73f9072f76f6c6217c04bd5de91be3135d
sofia1098 <sofianunez000@gmail.com>
2025-07-22 18:10:28 +0000

    fin

--- structures_typedef/4-main.c
+++ structures_typedef/4-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -12,5 +13,8 @@ int main(void)
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free(my_dog->name);
+    free(my_dog->owner);
+    free(my_dog);
     return (0);
 }
--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,41 +7,41 @@ dog_t
 {
 	dog_t *d;
 	char *new_name, *new_owner;
-	int len_n = 0;
+/*	int len_n = 0;
 	int len_o = 0;
-	int i;
+	int i = 0;
 
 	for (len_n = 0; name[len_n] != '\0'; len_n++)
 		;
 
 	for (len_o = 0; owner[len_o] != '\0'; len_o++)
-		;
+		;*/
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+	new_name = strdup(name);
+	new_owner = strdup(owner);
+	/*new_name = malloc(len_n + 1);
 
-	new_name = malloc(len_n + 1);
 	if (new_name == NULL)
 	{
-		free(new_name);
 		free(d);
 		return (NULL);
 	}
 
 	for (i = 0; name[i] != '\0'; i++)
 		new_name[i] = name[i];
-
 	new_owner = malloc(len_o + 1);
 	if (new_owner == NULL)
 	{
-		free(new_owner);
+		free(new_name);
 		free(d);
 		return (NULL);
 	}
 	for (i = 0; owner[i] != '\0'; i++)
 		new_owner[i] = owner[i];
-
+	*/
 	d->name = new_name;
 	d->age = age;
 	d->owner = new_owner;
