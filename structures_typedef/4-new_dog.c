1469fe82478eed41c0b9b4ee6665bab5724e7f96
sofia1098 <sofianunez000@gmail.com>
2025-07-22 00:16:49 +0000

    cuatro

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include "dog.h"
+#include <stdlib.h>
+
+dog_t
+*new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	char *new_name;
+	char *new_owner;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	new_name = malloc(strlen(name) + 1);
+	if (new_name == NULL)
+	{
+		free(new_name);
+		free(d);
+		return (NULL);
+	}
+	strcpy(new_name, name);
+
+	new_owner = malloc(strlen(owner) + 1);
+
+	if (new_owner == NULL)
+	{
+		free(new_owner);
+		free(d);
+		return (NULL);
+	}
+	strcpy(new_owner, owner);
+
+	d->name = new_name;
+	d->age = age;
+	d->owner = new_owner;
+
+	return (d);
+}
