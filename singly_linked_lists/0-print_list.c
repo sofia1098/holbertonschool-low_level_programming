dc9e59ee24bfe76052079843d9f08181be50d3e7
sofia1098 <sofianunez000@gmail.com>
2025-07-23 14:43:13 +0000

    cero

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,13 +9,16 @@ print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+		{
+			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 
 		h = h->next;
 		nodos++;
-
 	}
 	return (nodos);
 }
