c688e7b2db5da9749cba2a56c8eda5df4109650b
sofia1098 <sofianunez000@gmail.com>
2025-07-23 18:29:26 +0000

    archivos inicio

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t
+print_dlistint(const dlistint_t *h)
+{
+	int i;
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,9 @@
+0 - Funcion que imprime los elementos de una lista
+1 - 
+2 -
+3 -
+4 -
+5 -
+6 -
+7 -
+8 -
