ce0e840c0a4cd016092dbf59d8b7f1f10458528a
sofia1098 <sofianunez000@gmail.com>
2025-07-23 19:23:54 +0000

    cero

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,17 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 size_t
 print_dlistint(const dlistint_t *h)
 {
-	int nodos = 0;
+	int nd = 0;
 
-	if (h == NULL)
-		return (NULL);
-
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nd++;
+	}
+	return (nd);
 
 }
