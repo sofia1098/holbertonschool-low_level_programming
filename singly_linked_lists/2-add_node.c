bc7782142d6383dc40806501ebf53890ce2a5741
sofia1098 <sofianunez000@gmail.com>
2025-07-23 15:43:31 +0000

    dos

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,12 +1,35 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <string.h>
 
-list_t *add_node(list_t **head, const char *str)
+list_t
+*add_node(list_t **head, const char *str)
 {
-	int x;
+	list_t *nuevo;
 
-	char *cpy;
+	if (str == NULL)
+		return (NULL);
 
-	while (h = NULL)
+	nuevo = malloc(sizeof(list_t));
+	if (nuevo == NULL)
+		return (NULL);
+
+	nuevo->str = strdup(str); /*copia el string ya con malloc*/
+	if (nuevo->str == NULL)
+	{
+		free(nuevo);
+		return (NULL);
+	}
+
+	nuevo->len = strlen(str);
+	nuevo->next = *head; /*puntero al head anterior*/
+	*head = nuevo; /*actualiza el puntero head al nuevo nodo*/
+
+	/**
+	 * lista antes: head - [A] - [B] - [C] - NULL
+	 * lista despues: head - [NEW] - [A] - [B] - [C] - NULL
+	 */
+
+	return (nuevo);
 
 }
