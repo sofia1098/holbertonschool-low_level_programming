e68d305bde415b5a4c1e2855b0ace838b11836fb
sofia1098 <sofianunez000@gmail.com>
2025-07-23 18:14:19 +0000

    cuatro

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+
+void
+free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+	free(head->str);
+	free(head);
+	head = temp;
+	}
+}
