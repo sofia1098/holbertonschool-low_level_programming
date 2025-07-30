7cfb440e9d0ef62d53608830e1f2a7ab07fe1581
sofia1098 <sofianunez000@gmail.com>
2025-07-30 21:37:13 +0000

    casi tres

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,6 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
 
 int
 hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -7,42 +9,48 @@ hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	char *copia;
 
-	hash_table_set *new, *actual;
-	
-	if (key == NULL || *key == '\0'|| ht == NULL || value == NULL)
+	hash_node_t *new, *actual; /*ptr a nodo*/
+
+	if (key == NULL || *key == '\0' || ht == NULL || value == NULL)
 		return (0);
 
-	copia = strup(value);/*copia con malloc un string*/
-	if(!copia)
+	copia = strdup(value);/*copia con malloc un string*/
+	if (!copia)
 		return (0);
 
-	i = key_index((const unsigned char *)key,ht->size);/*casteo*/
-	------
+	idx = key_index((const unsigned char *)key, ht->size);/*casteo*/
+	actual = ht->array[idx];
 
-	actual = ht->array[i];
-
-	while (actual != NULL) /*recorre nodos hasta el final*/
+	while (actual != NULL) /*si la lista en ese idx no esta vacia*/
 	{
-		if (actual->key == 0)
+		if (strcmp(actual->key, key) == 0)/*compara dos str y si son iguales r(0)*/
 		{
-
+			free(actual->value); /*Si key existe, se reemplaza el valor anterior xnew*/
+			actual->value = copia;
+			return (1);
 		}
+		actual = actual->next;/*me muevo al sig nodo*/
 	}
- /* Crear un nuevo nodo para la clave */
-	new = malloc(sizeof(ht));
+
+	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
-		free(new);
+		free(copia);
 		return (0);
 	}
-	/*duplicar clave*/
+
+	/*duplicar clave en nuevo nodo*/
 	new->key = strdup(key);
 	if (new->key == NULL)
 	{
+		free(copia);
 		free(new);
 		return (0);
 	}
-
-	new->value = strdup ()
+/*conecto el nuevo nodo como primero en la lista*/
+	new->value = copia;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+	return (1);
 
 }
