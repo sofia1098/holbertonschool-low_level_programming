576ff39a27ace9c6e3f3d5895371918674de5a90
sofia1098 <sofianunez000@gmail.com>
2025-07-30 21:48:12 +0000

    cuatro

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+#include <stdlib.h> 
+#include <string.h>
+
+char
+*hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *nodo;
+	unsigned long int idx;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	nodo = ht->array[idx];
+
+	while (nodo) /*no es nulo*/
+	{
+		if (strcmp(nodo->key, key) == 0)
+		return (nodo->value);
+		nodo = nodo->next;
+    	}
+	return (NULL);
+}
